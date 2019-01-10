9c796659dd88db91ca945a43dfd8ce88dad0ac7a
yaosir0317 <1316231847@qq.com>
2019-01-10 20:48:13 +0800

    commit

---
+++ Flask/mytest.py
@@ -0,0 +1,114 @@
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import jsonify
+from flask import send_file
+from flask import request
+from flask import session
+from flask import Markup
+from flask import views
+from flask import Blueprint
+from functools import wraps
+
+# AssertionError: View function mapping is overwriting an existing endpoint function: inner
+
+api = Flask(__name__)
+api.secret_key = "good night!"
+
+
+def login_access(fun):
+    @wraps(fun)
+    def inner(*args, **kwargs):
+        if not session.get("user_key"):
+            return redirect("/login")
+        return fun(*args, **kwargs)
+    return inner
+
+
+@api.template_global()
+def func(a, b):
+    return a+b
+
+
+@api.template_filter()
+def fil(a, b, c):
+    return a+b+c
+
+
+tag = Markup("<input type='text' name='username'>")
+
+
+@api.route("/")
+@login_access
+def index():
+    return render_template("index.html", tag=tag)
+
+
+@api.route("/login", methods=["POST", "GET"])
+def login():
+    print(request.method)
+    print(request.form)
+    print(request.form.to_dict())
+    print(request.args)
+    print(request.url)
+    print(request.path)
+    print(request.host)
+    print(request.cookies)
+    if request.method == "GET":
+        print("=========================================")
+        return render_template("login.html")
+    elif request.form.to_dict().get("username") == "123" and request.form.to_dict().get("password") == "123":
+        session["user_key"] = "The philosophers have only interpreted the world in various ways" \
+                              " - the point however is to change it"
+        return redirect("/")
+    return "登录失败"
+
+
+@api.route("/home")
+@login_access
+def home():
+    ret = {"name": "yao"}
+    return jsonify(ret)
+
+
+@api.route("/file")
+@login_access
+def file():
+    return send_file("D:\腾讯QQ文件\段子\girls.jpg")
+
+
+@api.route("/req")
+@login_access
+def req():
+    print(request.path)
+    return render_template("req.html")
+
+
+class Book(views.MethodView):
+    def get(self, num):
+        return render_template("req.html", num=num)
+
+    def post(self, num):
+        return "123"
+
+
+api.add_url_rule("/book/<num>", view_func=Book.as_view("my_book"))
+
+
+app01 = Blueprint("app01", __name__, url_prefix="/blue")
+
+
+@app01.route("/blue")
+def func_app():
+    return "blueprint"
+
+
+api.register_blueprint(app01)
+
+
+@api.errorhandler(404)
+def err(error_info):
+    return error_info
+
+
+api.run(debug=True)
---
+++ Flask/test.py
@@ -0,0 +1,107 @@
+import time
+
+
+def outer(fun):
+    def inner(*args, **kwargs):
+        start = time.time()
+        ret = fun(*args, **kwargs)
+        end = time.time()
+        print(end - start)
+        return ret
+    return inner
+
+
+@outer
+def quicksort(list):
+    if len(list) < 2:
+        return list
+    else:
+        mid_num = list[0]
+        large_num = [i for i in list[1:] if i > mid_num]
+        sm_num = [i for i in list[1:] if i <= mid_num]
+        result = quicksort(sm_num) + [mid_num] + quicksort(large_num)
+        return result
+
+
+# quicksort([1, 8, 4, 6, 7, 12, 9, 5, 478, 92, 5, 6, 5, 4, 1, 8, 4, 6, 7, 12, 9, 5, 478, 92, 5, 6, 5, 4])
+
+# 0.0009999275207519531
+
+# @outer
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         n = 0
+#         result = []
+#         while n < len(nums):
+#             a = nums[n]
+#             for num in nums[n + 1:]:
+#                 if (a + num) == target:
+#                     ret = []
+#                     for i, j in enumerate(nums):
+#                         if j == a or j == num:
+#                             ret.append(i)
+#                     result.append(ret)
+#             n += 1
+#         return result or "没有符合的结果"
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         hashmap = {}
+#         for index, num in enumerate(nums):
+#             to_find = target - num
+#             if to_find in hashmap:
+#                 return [index, hashmap[to_find]]
+#             hashmap[num] = index
+#         return None
+
+
+# class Solution(object):
+#     def reverse(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+#         result = 0
+#         if x*(-1) > x:
+#             result += int(str(x)[1:][::-1])*(-1)
+#         else:
+#             result += int(str(x)[::-1])
+#
+#         if result > (-2)**31 and result < (2**31 - 1):
+#             return result
+#         return 0
+#
+#
+# obj = Solution()
+# print(obj.reverse(1534236469))
+
+
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        half = 0
+        while half < x:
+            half = half*10 + x % 10
+            x //= 10
+        return x == half or x == half//10
+
+
+obj = Solution()
+print(obj.isPalindrome(1211121))
